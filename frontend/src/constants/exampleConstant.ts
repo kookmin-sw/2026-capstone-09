97e981f8971c95379eea908c836c07cfe42b4008
pkm021118 <pkm021117@gmail.com>
2026-04-15 12:34:06 +0900

    chore: 프로젝트 상세 예제 상수 추가

--- frontend/src/constants/exampleConstant.ts
+++ frontend/src/constants/exampleConstant.ts
@@ -1,5 +1,16 @@
+import type { OnlineUserTypes } from '@/app/(project)/projects/[projectId]/_component/OnlineUsers';
+
 export const EXAMPLE_PROJECT_SIDEBAR_PROFILE = {
   projectName: '플로밋 기획',
   userName: '황수민',
   userEmail: 'tnals655@naver.com',
 } as const;
+
+export const EXAMPLE_ONLINE_USERS: OnlineUserTypes[] = [
+  { name: '황수민', email: 'tnals655@kookmin.ac.kr' },
+  { name: '박건민', email: 'parkkunmin@kookmin.ac.kr' },
+  { name: '윤신지', email: 'shinji@kookmin.ac.kr' },
+  { name: '백채린', email: 'chaerin@kookmin.ac.kr' },
+  { name: '윤성욱', email: 'seonguk@kookmin.ac.kr' },
+  { name: '박정은', email: 'jeongeun@kookmin.ac.kr' },
+];
