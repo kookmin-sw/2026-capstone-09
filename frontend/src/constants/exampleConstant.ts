9c6ac322202030a65b3408c33184463b75a73a23
pkm021118 <pkm021117@gmail.com>
2026-04-15 16:22:33 +0900

    chore: 프로젝트 상세 예제 데이터 수정

--- frontend/src/constants/exampleConstant.ts
+++ frontend/src/constants/exampleConstant.ts
@@ -1,5 +1,3 @@
-import type { OnlineUserTypes } from '@/app/(project)/projects/[projectId]/_component/OnlineUsers';
-
 export const EXAMPLE_PROJECT_SIDEBAR_PROFILE = {
   projectName: '플로밋 기획',
   userName: '황수민',
@@ -13,11 +11,11 @@ export const EXAMPLE_PROJECT_DETAIL_LINKS = [
   { id: 'link-4', label: 'Vercel', href: 'https://vercel.com/dashboard' },
 ] as const;
 
-export const EXAMPLE_ONLINE_USERS: OnlineUserTypes[] = [
+export const EXAMPLE_USERS = [
   { name: '황수민', email: 'tnals655@kookmin.ac.kr' },
   { name: '박건민', email: 'parkkunmin@kookmin.ac.kr' },
   { name: '윤신지', email: 'shinji@kookmin.ac.kr' },
   { name: '백채린', email: 'chaerin@kookmin.ac.kr' },
   { name: '윤성욱', email: 'seonguk@kookmin.ac.kr' },
   { name: '박정은', email: 'jeongeun@kookmin.ac.kr' },
-];
+] as const;
