704081ec0a6f90724945dfc37856cfad559ae032
pkm021118 <pkm021117@gmail.com>
2026-04-15 13:37:37 +0900

    style: 프로젝트 상세 헤더 스타일 수정

--- frontend/src/app/(project)/projects/[projectId]/_component/ProjectDetailHeader.tsx
+++ frontend/src/app/(project)/projects/[projectId]/_component/ProjectDetailHeader.tsx
@@ -13,7 +13,7 @@ export const ProjectDetailHeader = ({
   onViewChange,
 }: ProjectDetailHeaderProps) => {
   return (
-    <header className="relative z-10 flex h-14 shrink-0 items-center justify-between overflow-visible border-b border-line-soft bg-static-white px-4 pt-4 pb-3">
+    <header className="relative z-10 flex h-14 shrink-0 items-center justify-between overflow-visible border-b border-line-soft bg-static-white px-4 pt-4.5 pb-4">
       <ProjectViewTabs activeView={activeView} onViewChange={onViewChange} />
       <OnlineUsers users={onlineUsers} />
     </header>
