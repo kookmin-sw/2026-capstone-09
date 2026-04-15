95a6cf5ee1f3b25c31e94ed6fd7e8bc7ca169f97
pkm021118 <pkm021117@gmail.com>
2026-04-15 16:22:00 +0900

    refactor: 프로젝트 상세 페이지 엔트리 정리

--- frontend/src/app/(project)/projects/[projectId]/page.tsx
+++ frontend/src/app/(project)/projects/[projectId]/page.tsx
@@ -1,5 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+
+import { EXAMPLE_USERS } from '@/constants/exampleConstant';
+import type { UserInfo } from '@/components/commons/UserAvatarGroup';
+import { ProjectDetailHeader } from './_component/ProjectDetailHeader';
+import { ProjectDetailLinks } from './_component/ProjectDetailLinks';
 import { ProjectDetailPageContent } from './_component/ProjectDetailPageContent';
+import type { ProjectViewTypes } from './_component/ProjectViewTabs';
 
 export default function ProjectDetailPage() {
-  return <ProjectDetailPageContent />;
+    const [activeView, setActiveView] = useState<ProjectViewTypes>('node-flow');
+
+    return (
+        <div className="flex h-full w-full flex-1 flex-col overflow-hidden">
+            <ProjectDetailHeader
+                activeView={activeView}
+                onlineUsers={EXAMPLE_USERS as readonly UserInfo[]}
+                onViewChange={setActiveView}
+            />
+            <ProjectDetailLinks />
+            <ProjectDetailPageContent activeView={activeView} />
+        </div>
+    );
 }
