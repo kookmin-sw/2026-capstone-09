9dff7b226d3aee6713b23b981e6159b602b43693
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-04-19 18:35:39 +0900

    HOTFIX: 헬스체크 api 403 오류 해결

--- backend/flowmeet-auth/src/main/java/kr/flowmeet/auth/security/SecurityWhiteList.java
+++ backend/flowmeet-auth/src/main/java/kr/flowmeet/auth/security/SecurityWhiteList.java
@@ -1,6 +1,6 @@
 package kr.flowmeet.auth.security;
 
 public class SecurityWhiteList {
-    public static final String[] PUBLIC_WHITE_LIST = { "/auth/", "/test/**" };
+    public static final String[] PUBLIC_WHITE_LIST = { "/auth/", "/test/**", "/actuator/health", "/actuator/health/**" };
     public static final String[] SWAGGER_WHITE_LIST = { "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs/**" };
 }
