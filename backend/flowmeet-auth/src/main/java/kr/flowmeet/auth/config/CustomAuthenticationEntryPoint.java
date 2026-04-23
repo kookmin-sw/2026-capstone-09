221d6f90ec51c47522dfa6435ecc7a142663f571
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-04-23 23:22:54 +0900

    HOTFIX: SpringBoot 4.0.x 버전 대응 ObjectMapper 변경

--- backend/flowmeet-auth/src/main/java/kr/flowmeet/auth/config/CustomAuthenticationEntryPoint.java
+++ backend/flowmeet-auth/src/main/java/kr/flowmeet/auth/config/CustomAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package kr.flowmeet.auth.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -14,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 @RequiredArgsConstructor
