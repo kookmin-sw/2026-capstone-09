ac3ea005349b71c9c6b114538cb4185258085b33
wngktjd13 <seonguk3553@kookmin.ac.kr>
2026-05-29 11:52:58 +0900

    HOTFIX: RestClient PATCH 요청 실패 해결

--- mcp-server/src/main/java/kr/flowmeet/mcpserver/config/BackendClientConfig.java
+++ mcp-server/src/main/java/kr/flowmeet/mcpserver/config/BackendClientConfig.java
@@ -3,7 +3,7 @@ package kr.flowmeet.mcpserver.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 import java.time.Duration;
@@ -13,9 +13,9 @@ public class BackendClientConfig {
 
     @Bean
     public RestClient backendRestClient(@Value("${backend.url}") String backendUrl) {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
         factory.setConnectTimeout(Duration.ofSeconds(3));
-        factory.setReadTimeout(Duration.ofSeconds(10));
+        factory.setConnectionRequestTimeout(Duration.ofSeconds(10));
 
         return RestClient.builder()
                 .baseUrl(backendUrl)
